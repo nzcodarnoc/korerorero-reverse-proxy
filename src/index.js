8b036d30c1536400e04086d2ea6aea61ffc9bf62
Conrad Johnston <conrad@darnoc.co.nz>
2020-03-16 17:07:50 +1300

    Add mouth shapes to the composition

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ var { createProxyMiddleware } = require('http-proxy-middleware');
 var app = express();
 
 app.use(createProxyMiddleware('/tts', {
-    target: 'http://marytts:59125',
+    target: 'http://tts:59125',
     pathRewrite: {
       '^/tts': ''
     }
 }));
+
+app.use(createProxyMiddleware('/mouth-shapes', {
+  target: 'http://mouth-shapes:3000',
+  pathRewrite: {
+    '^/mouth-shapes': ''
+  }
+}));
+
 
 app.listen(3000);
