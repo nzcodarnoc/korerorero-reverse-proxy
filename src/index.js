35f1a944b3ef37e1ee4a0c8254a6535ba6e5eecf
Conrad Johnston <conrad@darnoc.co.nz>
2020-03-24 17:05:58 +1300

    Linting tweaking 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var { createProxyMiddleware } = require('http-proxy-middleware');
+var express = require('express')
+var { createProxyMiddleware } = require('http-proxy-middleware')
 
-var app = express();
+var app = express()
 
-app.use(createProxyMiddleware('/tts', {
+app.use(
+  createProxyMiddleware('/tts', {
     target: 'http://marytts:59125',
     pathRewrite: {
-      '^/tts': ''
-    }
-}));
+      '^/tts': '',
+    },
+  })
+)
 
-app.listen(3000);
+app.listen(3000)
