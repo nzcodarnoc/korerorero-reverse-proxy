a706faaa2f3b36c35dc1c7934ffdec4aad615ea9
Conrad Johnston <conrad@darnoc.co.nz>
2020-04-08 16:56:57 +1200

    Security hardening, and remove attempts at linting because the solution was too complicated.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
-var express = require('express')
-var { createProxyMiddleware } = require('http-proxy-middleware')
+const express = require("express");
+const helmet = require("helmet");
 
-var app = express()
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-app.use(createProxyMiddleware('/tts', {
-    target: 'http://tts:59125',
+const app = express();
+app.use(helmet());
+app.use(helmet.xssFilter());
+app.disable("x-powered-by");
+
+app.use(
+  createProxyMiddleware("/tts", {
+    target: "http://tts:59125",
     pathRewrite: {
-      '^/tts': '',
+      "^/tts": "",
     },
   })
-)
+);
 
-app.use(createProxyMiddleware('/mouth-shapes', {
-  target: 'http://mouth-shapes:3000',
-  pathRewrite: {
-    '^/mouth-shapes': ''
-  }
-}));
+app.use(
+  createProxyMiddleware("/mouth-shapes", {
+    target: "http://mouth-shapes:3000",
+    pathRewrite: {
+      "^/mouth-shapes": "",
+    },
+  })
+);
+
+app.use(
+  createProxyMiddleware("/orchestration", {
+    target: "http://orchestration:3001",
+    pathRewrite: {
+      "^/orchestration": "",
+    },
+  })
+);
 
 app.listen(3000);
